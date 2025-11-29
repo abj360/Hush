0069f78cec0e952927cad8622bd132cd38faabf9
peter abolaji <oluwatoyosi.abolaji25@gmail.com>
2025-11-29 23:30:25 +0300

    Updated cmakefile

---
+++ build/CMakeFiles/HushMe.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HushMe.dir/src/dsp_utils.cpp.o"
+  "CMakeFiles/HushMe.dir/src/dsp_utils.cpp.o.d"
+  "CMakeFiles/HushMe.dir/src/hybrid_processor.cpp.o"
+  "CMakeFiles/HushMe.dir/src/hybrid_processor.cpp.o.d"
+  "CMakeFiles/HushMe.dir/src/main.cpp.o"
+  "CMakeFiles/HushMe.dir/src/main.cpp.o.d"
+  "CMakeFiles/HushMe.dir/src/ml_processor.cpp.o"
+  "CMakeFiles/HushMe.dir/src/ml_processor.cpp.o.d"
+  "CMakeFiles/HushMe.dir/src/vad.cpp.o"
+  "CMakeFiles/HushMe.dir/src/vad.cpp.o.d"
+  "HushMe"
+  "HushMe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HushMe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/HushMe.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for HushMe.
+# This may be replaced when dependencies are built.
