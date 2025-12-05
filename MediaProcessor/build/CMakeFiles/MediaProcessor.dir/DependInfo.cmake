4852cb8ce0d41e2b56aab68b96cf8252d1bc1067
peter abolaji <oluwatoyosi.abolaji25@gmail.com>
2025-12-05 21:04:42 +0300

    adjusted static files

---
+++ MediaProcessor/build/CMakeFiles/MediaProcessor.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/peter/Hush/MediaProcessor/src/AudioProcessor.cpp" "CMakeFiles/MediaProcessor.dir/src/AudioProcessor.cpp.o" "gcc" "CMakeFiles/MediaProcessor.dir/src/AudioProcessor.cpp.o.d"
+  "/home/peter/Hush/MediaProcessor/src/CommandBuilder.cpp" "CMakeFiles/MediaProcessor.dir/src/CommandBuilder.cpp.o" "gcc" "CMakeFiles/MediaProcessor.dir/src/CommandBuilder.cpp.o.d"
+  "/home/peter/Hush/MediaProcessor/src/ConfigManager.cpp" "CMakeFiles/MediaProcessor.dir/src/ConfigManager.cpp.o" "gcc" "CMakeFiles/MediaProcessor.dir/src/ConfigManager.cpp.o.d"
+  "/home/peter/Hush/MediaProcessor/src/DeepFilterCommandBuilder.cpp" "CMakeFiles/MediaProcessor.dir/src/DeepFilterCommandBuilder.cpp.o" "gcc" "CMakeFiles/MediaProcessor.dir/src/DeepFilterCommandBuilder.cpp.o.d"
+  "/home/peter/Hush/MediaProcessor/src/Engine.cpp" "CMakeFiles/MediaProcessor.dir/src/Engine.cpp.o" "gcc" "CMakeFiles/MediaProcessor.dir/src/Engine.cpp.o.d"
+  "/home/peter/Hush/MediaProcessor/src/FFmpegSettingsManager.cpp" "CMakeFiles/MediaProcessor.dir/src/FFmpegSettingsManager.cpp.o" "gcc" "CMakeFiles/MediaProcessor.dir/src/FFmpegSettingsManager.cpp.o.d"
+  "/home/peter/Hush/MediaProcessor/src/HardwareUtils.cpp" "CMakeFiles/MediaProcessor.dir/src/HardwareUtils.cpp.o" "gcc" "CMakeFiles/MediaProcessor.dir/src/HardwareUtils.cpp.o.d"
+  "/home/peter/Hush/MediaProcessor/src/Utils.cpp" "CMakeFiles/MediaProcessor.dir/src/Utils.cpp.o" "gcc" "CMakeFiles/MediaProcessor.dir/src/Utils.cpp.o.d"
+  "/home/peter/Hush/MediaProcessor/src/VideoProcessor.cpp" "CMakeFiles/MediaProcessor.dir/src/VideoProcessor.cpp.o" "gcc" "CMakeFiles/MediaProcessor.dir/src/VideoProcessor.cpp.o.d"
+  "/home/peter/Hush/MediaProcessor/src/main.cpp" "CMakeFiles/MediaProcessor.dir/src/main.cpp.o" "gcc" "CMakeFiles/MediaProcessor.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ MediaProcessor/build/CMakeFiles/MediaProcessor.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for MediaProcessor.
+# This may be replaced when dependencies are built.
